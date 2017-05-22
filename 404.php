4234e651e907d995f497fc52917411a101242455
Steve North <tex0gen@gmail.com>
2017-05-22 23:49:48 +0100

    Added 404 page. Changed theme to be a parent theme.

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div class="container">
+	<div class="row">
+		<div class="col-12 contain-section">
+			<h1>Error 404 - Page Not Found</h1>
+			<p>Sorry, the page you have requested has been moved or deleted. <a href="<?= get_bloginfo('url'); ?>">Click here</a> to be taken back to the home page.</p>
+		</div>
+	</div>
+</div>
+<?php get_footer();
--- functions.php
+++ functions.php
@@ -14,12 +14,12 @@ function theme_enqueue_styles() {
 	// wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto:400,700' );
 
 	// CSS
-	wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/assets/css/main.css' );
+	wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.css' );
 
 	// Javascript
-	wp_enqueue_script( 'tether', get_stylesheet_directory_uri() . '/assets/js/tether.min.js', array(), null, true );
-	wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . '/assets/sass/bootstrap/dist/js/bootstrap.min.js', array('jquery', 'tether'), null, true );
-	wp_enqueue_script( 'mainjs', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'), null, true );
+	wp_enqueue_script( 'tether', get_template_directory_uri() . '/assets/js/tether.min.js', array(), null, true );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/sass/bootstrap/dist/js/bootstrap.min.js', array('jquery', 'tether'), null, true );
+	wp_enqueue_script( 'mainjs', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, true );
 
 }
 
