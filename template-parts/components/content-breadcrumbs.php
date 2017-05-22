2a6e991c7b02e5550c5cb59556d16755d3cfbd31
Steve North <tex0gen@gmail.com>
2017-05-22 23:53:29 +0100

    Changed breadcrumb to only show if Yoast SEO breadcrumbs are switched on.

--- template-parts/components/content-breadcrumbs.php
+++ template-parts/components/content-breadcrumbs.php
@@ -1,13 +1,9 @@
-<?php if ( !is_front_page() ) { ?>
+<?php if ( !is_front_page() && function_exists('yoast_breadcrumb') ) { ?>
 	<section class="breadcrumb">
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<?php
-					if ( function_exists('yoast_breadcrumb') ) {
-						yoast_breadcrumb('<div class="breadcrumbs">','</div>');
-					}
-					?>
+					<?php yoast_breadcrumb('<div class="breadcrumbs">','</div>'); ?>
 				</div>
 			</div>
 		</div>
