03085118d97dcd5a6134c351cecd0508f6539b21
Steve <steven@visarc.uk>
2017-10-23 16:24:15 +0100

    Removed live checking and moved to themestrap-child instead.

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -1,17 +1,4 @@
 <?php
-
-// Allows robot indexing after dev database has been pushed
-$live_url = ''; // Must match WordPress live URL
-if ($live_url != '') {
-	if ( function_exists(get_field) ) {
-		if ( get_option('blog_public') === 0 ) {
-			update_option('blog_public', 1);
-		}
-	}
-} else {
-	echo '<pre>Please add the live site URL to themestrap/inc/theme-options (Line 4)</pre>';
-}
-
 require_once('settings/image-sizes.php');
 require_once('settings/widgets.php');
 
