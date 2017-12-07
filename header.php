4c11734f87b83289433aaae897c480b24070b424
Steve North <tex0gen@gmail.com>
2017-12-07 11:38:58 +0000

    Added title element, added html decleration, added language, removed bootstrap core from main.scss

--- header.php
+++ header.php
@@ -1,7 +1,9 @@
-<html>
+<!DOCTYPE html>
+<html lang="en-US">
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title><?= wp_title() ?></title>
 		<?php wp_head(); ?>
 	</head>
 	<body <?= body_class(); ?>>
