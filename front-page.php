41adf95575898274e95ea16598fcbe0ab4b50d24
Steve North <tex0gen@gmail.com>
2017-12-22 13:45:36 +0000

    Removed chevron test

--- front-page.php
+++ front-page.php
@@ -5,9 +5,6 @@ if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
 		get_template_part( 'template-parts/content', 'flex' );
-		?>
-		<i class="fa fa-chevron-left"></i>
-		<?php
 	endwhile;
 endif;
 
