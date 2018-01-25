306017f17f0467049c37fa4685b58ebbcab21cd5
Steve North <tex0gen@gmail.com>
2018-01-25 22:16:11 +0000

    Clean up WP output

--- functions.php
+++ functions.php
@@ -30,3 +30,33 @@ require_once 'inc/settings/image-sizes.php';
 if ( class_exists( 'WooCommerce' ) ) {
   require_once 'inc/woocommerce/woocommerce.php';
 }
+
+add_filter( 'style_loader_tag',  'clean_style_tag'  );
+add_filter( 'script_loader_tag', 'clean_script_tag'  );
+
+function clean_style_tag( $input ) {
+  preg_match_all( "!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!", $input, $matches );
+  if ( empty( $matches[2] ) ) {
+    return $input;
+  }
+  // Only display media if it is meaningful
+  $media = $matches[3][0] !== '' && $matches[3][0] !== 'all' ? ' media="' . $matches[3][0] . '"' : '';
+  return '<link rel="stylesheet" href="' . $matches[2][0] . '"' . $media . '>' . "\n";
+}
+
+function clean_script_tag( $input ) {
+  $input = str_replace( "type='text/javascript' ", '', $input );
+  return str_replace( "'", '"', $input );
+}
+
+
+function remove_head_scripts() { 
+	remove_action('wp_head', 'wp_print_scripts'); 
+	remove_action('wp_head', 'wp_print_head_scripts', 9); 
+	remove_action('wp_head', 'wp_enqueue_scripts', 1);
+
+	add_action('wp_footer', 'wp_print_scripts', 5);
+	add_action('wp_footer', 'wp_enqueue_scripts', 5);
+	add_action('wp_footer', 'wp_print_head_scripts', 5); 
+} 
+add_action( 'wp_enqueue_scripts', 'remove_head_scripts' );
