819f250b14049fd4c348cf1105444606e854f484
Steve North <tex0gen@gmail.com>
2018-01-25 22:56:37 +0000

    remove query strings from resources. Remove WP version

--- functions.php
+++ functions.php
@@ -31,9 +31,7 @@ if ( class_exists( 'WooCommerce' ) ) {
   require_once 'inc/woocommerce/woocommerce.php';
 }
 
-add_filter( 'style_loader_tag',  'clean_style_tag'  );
-add_filter( 'script_loader_tag', 'clean_script_tag'  );
-
+// Clean style tags
 function clean_style_tag( $input ) {
   preg_match_all( "!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!", $input, $matches );
   if ( empty( $matches[2] ) ) {
@@ -43,13 +41,16 @@ function clean_style_tag( $input ) {
   $media = $matches[3][0] !== '' && $matches[3][0] !== 'all' ? ' media="' . $matches[3][0] . '"' : '';
   return '<link rel="stylesheet" href="' . $matches[2][0] . '"' . $media . '>' . "\n";
 }
+add_filter( 'style_loader_tag',  'clean_style_tag'  );
 
+// Clean script tags
 function clean_script_tag( $input ) {
   $input = str_replace( "type='text/javascript' ", '', $input );
   return str_replace( "'", '"', $input );
 }
+add_filter( 'script_loader_tag', 'clean_script_tag'  );
 
-
+// Move scripts to footer
 function remove_head_scripts() { 
 	remove_action('wp_head', 'wp_print_scripts'); 
 	remove_action('wp_head', 'wp_print_head_scripts', 9); 
@@ -60,3 +61,19 @@ function remove_head_scripts() {
 	add_action('wp_footer', 'wp_print_head_scripts', 5); 
 } 
 add_action( 'wp_enqueue_scripts', 'remove_head_scripts' );
+
+// Remove WP version
+function wpbeginner_remove_version() {
+	return '';
+}
+add_filter('the_generator', 'wpbeginner_remove_version');
+
+// remove wp version param from any enqueued scripts
+function vc_remove_wp_ver_css_js( $src ) {
+  if ( strpos( $src, 'ver=' ) ) {
+    $src = remove_query_arg( 'ver', $src );
+  }
+  return $src;
+}
+add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
