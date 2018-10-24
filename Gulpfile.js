3061ff82ad82449693b83a1b0841c1d02e6832c3
Steve <tex0gen@gmail.com>
2018-10-24 09:11:10 +0100

    Updated gulp file for JS minification

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,56 +10,73 @@ const uglify = require('gulp-uglify');
 const clean = require('gulp-clean');
 const postcss = require('gulp-postcss');
 const uncss = require('postcss-uncss');
+const imageOptim = require('gulp-imageoptim');
 
 // Comile SASS
 gulp.task('sass', function() {
   return gulp.src('./assets/sass/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    // .pipe(autoprefixer())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('./assets/build/css/'));
-
 });
 
 // Removes unused CSS
-gulp.task('purify', function() {
-  var plugins = [
-    uncss({
-      html: ['./**/*.php']
-    }),
-  ];
+// gulp.task('purify', function() {
+//   var plugins = [
+//     uncss({
+//       html: ['./**/*.php']
+//     }),
+//   ];
 
-  return gulp.src('./assets/build/css/main.min.css')
-    .pipe(postcss(plugins))
-    .pipe(gulp.dest('./assets/build/css/'));
-});
-
-// RM old minified CSS
-gulp.task('del-min-css', function(){
-  return gulp.src('./assets/build/css/main.min.css',{force: true})
-  .pipe(clean())
-});
-
-// Minify CSS
-// gulp.task('minify', ['del-min-css'], function() {
-//   gulp.src('./assets/build/css/main.css')
-//     .pipe(cleanCSS())
-//     .pipe(rename('main.min.css'))
+//   return gulp.src('./assets/build/css/main.min.css')
+//     .pipe(postcss(plugins))
 //     .pipe(gulp.dest('./assets/build/css/'));
 // });
 
+// RM old minified CSS
+// gulp.task('del-min-css', function(done){
+//   return gulp.src('./assets/build/css/main.min.css',{force: true})
+//   .pipe(clean())
+// });
+
+// Minify CSS
+gulp.task('minify', function() {
+  return gulp.src('./assets/build/css/main.css')
+    .pipe(cleanCSS())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('./assets/build/css/'));
+});
+
 // Concat & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src([
-      './assets/js/tether.min.js',
-      './assets/js/popper.js',
-      './assets/sass/bootstrap/dist/js/bootstrap.min.js',
-      './assets/js/owl.carousel.min.js',
-      './assets/js/main.js',
-      './assets/js/cookie-policy.js'
-    ])
-    .pipe(concat('scripts.js'))
+  return gulp.src([
+    './assets/js/tether.min.js',
+    './assets/js/popper.js',
+    './assets/sass/bootstrap/dist/js/bootstrap.min.js',
+    './assets/js/owl.carousel.min.js',
+    './assets/js/main.js',
+    './assets/js/cookie-policy.js'
+  ])
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest('./assets/build/js/'));
+});
+
+
+gulp.task('uglify', function() {
+  return gulp.src('./assets/build/js/scripts.js')
+    .pipe(uglify())
     .pipe(gulp.dest('./assets/build/js/'));
+});
+
+gulp.task('optimise', function(done) {
+  // gulp.src('../../uploads/**/**/*')
+  //   .pipe(imageOptim.optimize())
+  //   .pipe(gulp.dest('../../uploads'));
+  gulp.src('./')
+    .pipe(imageOptim.optimize().on('error', console.log('lol')))
+    .pipe(gulp.dest('./'));
+  done();
 });
 
 // Prep for ES6
@@ -76,7 +93,12 @@ gulp.task('scripts', function() {
 
 // Watch task
 gulp.task('default',function() {
-  gulp.watch(['assets/sass/**/*.scss', 'assets/js/**/*.js'], gulp.parallel('sass', 'scripts'));
+  gulp.watch(['assets/sass/**/*.scss', 'assets/js/**/*.js'], gulp.series('sass', 'scripts', 'minify', 'uglify'));
+});
+
+gulp.task('images', function(done) {
+  gulp.parallel('optimise');
+  done();
 });
 
 // gulp.task('build',function() {
