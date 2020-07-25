a6aa6dc6b6fcbcfb7977be7436f2508c860d2866
Steve North <tex0gen@gmail.com>
2020-07-25 15:04:58 +0100

    Added page-header options

--- page.php
+++ page.php
@@ -35,18 +35,12 @@ if (
 	if ( have_posts() ) :
 		while ( have_posts() ) :
 			the_post();
-			?>
-			<div class="page-header">
-				<div class="container">
-					<div class="row">
-						<div class="col-12">
-							<?php the_title('<h1>','</h1>'); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-			<?php get_template_part( 'template-parts/content', 'flex' ); ?>
-			<?php
+
+			if (!get_field('landing_page')) {
+				get_template_part('template-parts/components/content', 'page-header');
+			}
+
+			get_template_part( 'template-parts/content', 'flex' );
 		endwhile;
 	endif;
 }
---
+++ template-parts/components/content-page-header.php
@@ -0,0 +1,11 @@
+<?php $featured_image = wp_get_attachment_image_url( get_post_thumbnail_id(), 'large', false ) ; ?>
+<div class="page-header"<?= ($featured_image) ? ' style="background-image:url('.$featured_image.');"':''; ?>>
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <?php the_title('<h1>','</h1>'); ?>
+      </div>
+    </div>
+  </div>
+</div>
