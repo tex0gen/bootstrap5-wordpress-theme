dad7f29d51ca5922a28c8ec22b1d4e8c29a5f2c5
Steve <tex0gen@gmail.com>
2020-07-28 16:12:56 +0100

    Updates to slider. Added ES6 compiling.

--- template-parts/flex/slider.php
+++ template-parts/flex/slider.php
@@ -2,57 +2,52 @@
 $slides = get_sub_field('carousel');
 
 if ($slides) {
+	$carousel_type = get_sub_field('type');
+	$showcase = ($carousel_type === 'showcase') ? true:false;
 	$opts = array(
 		'animateOut' => (get_sub_field('transition') === 'linear') ? false:get_sub_field('transition'),
 		'autoplayTimeout' => get_sub_field('speed'),
 		'loop' => get_sub_field('loop'),
-		'items' => (int)get_sub_field('number_to_show'),
+		'items' => ($showcase) ? 3:(int)get_sub_field('number_to_show'),
 		'nav' => get_sub_field('indicators'),
-		'dots' => get_sub_field('navigation')
+		'dots' => get_sub_field('navigation'),
+		'center' => $showcase,
 	);
 ?>
-<section class="flex-carousel">
+<section class="flex-carousel<?= ($carousel_type) ? ' '.$carousel_type:''; ?>">
 	<div class="owl-carousel" data-carousel-options='<?= json_encode($opts) ?>'>
 		<?php
 		foreach ($slides as $key => $slide) {
 			?>
-			<div class="has-desktop<?= ($slide['mobile_image']) ? ' has-mobile':''; ?>">
+			<div class="has-desktop<?= ($slide['mobile_image']['ID']) ? ' has-mobile':''; ?>">
+				<?= wp_get_attachment_image( $slide['background_image']['ID'], 'full-width', false, array('class' => 'img-fluid desktop-img') ); ?>
+				<?= wp_get_attachment_image( $slide['mobile_image']['ID'], 'large', false, array('class' => 'img-fluid mobile-img') ); ?>
+				
 				<?php
-				if ( $slide['image'] ) {
-					echo wp_get_attachment_image( $slide['image']['ID'], 'slider', false, array('class' => 'img-fluid desktop-img') );
-				}
-
-				if ( $slide['mobile_image'] ) {
-					echo wp_get_attachment_image( $slide['mobile_image']['ID'], 'large', false, array('class' => 'img-fluid mobile-img') );
-				}
-
-				if ( get_sub_field( 'call_to_action' ) ) {
-					$align = $slide['alignment'];
-					switch ($align) {
+				if ($carousel_type === 'cta') {
+					$foreground_image = $slide['foreground_image'];
+					$alignment = $slide['alignment'];
+					switch ($alignment) {
 						case 'left':
-							$align = '';
+							$textalign = '';
 							break;
 						case 'center':
-							$align = ' offset-md-3 text-center';
+							$textalign = ' offset-md-3';
 							break;
 						case 'right':
-							$align = ' offset-md-6 text-right';
+							$textalign = ($foreground_image) ? ' order-md-1':' offset-md-6';
 							break;
 						default:
-							$align = '';
+							$textalign = '';
 							break;
 					}
 					?>
 					<div class="carousel-text">
 						<div class="container">
 							<div class="row align-items-center">
-								<div class="col-12 col-md-6<?= $align ?>">
+								<div class="col-12 col-md-6<?= $textalign ?>">
 									<?php
-									if ($slide['title']) {
-										echo '<h2>'.$slide['title'].'</h2>';
-									}
-
-									if ($slide['title']) {
+									if ($slide['text']) {
 										echo $slide['text'];
 									}
 									?>
@@ -67,6 +62,11 @@ if ($slides) {
 										?>
 									</div>
 								</div>
+								<?php if ($foreground_image) { ?>
+									<div class="col-12 col-md-6">
+										<?= wp_get_attachment_image( $foreground_image, 'large', false, array('class' => 'img-fluid') ); ?>
+									</div>
+								<?php } ?>
 							</div>
 						</div>
 					</div>
