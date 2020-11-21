00b526da8f6fc8f808f7f0f956da15d35f06731e
Steve North <tex0gen@gmail.com>
2020-11-21 23:18:22 +0000

    Added editor config. Changed some files to apply editorconfig.

--- functions.php
+++ functions.php
@@ -3,18 +3,18 @@
 // ------------------
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 5 );
 function theme_enqueue_styles() {
-  // CSS
-  // This will return shortly..
-  // $url = get_bloginfo('url');
-  // if (strpos($url, 'localhost') || strpos($url, 'dev.')) {
-  //   wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/build/css/main.css' );
-  // } else {
-    wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/build/css/main.min.css' );
-  // }
+	// CSS
+	// This will return shortly..
+	// $url = get_bloginfo('url');
+	// if (strpos($url, 'localhost') || strpos($url, 'dev.')) {
+	//   wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/build/css/main.css' );
+	// } else {
+	wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/build/css/main.min.css' );
+	// }
 
-  // Javascript
-  // ADD ANY LOCAL SCRIPTS TO THE GULP FILE SO THAT THEY CAN BE CONCATENATED AND MINIFIED
-  wp_enqueue_script( 'mainjs', get_template_directory_uri() . '/assets/build/js/scripts.js', array('jquery'), null, false );
+	// Javascript
+	// ADD ANY LOCAL SCRIPTS TO THE GULP FILE SO THAT THEY CAN BE CONCATENATED AND MINIFIED
+	wp_enqueue_script( 'mainjs', get_template_directory_uri() . '/assets/build/js/scripts.js', array('jquery'), null, false );
 }
 
 // Include Setup
@@ -31,6 +31,6 @@ require_once 'inc/plugins/tgm-ip.php';
 
 // Include Woocommerce Customisations
 if ( class_exists( 'WooCommerce' ) ) {
-  require_once 'inc/woocommerce/woocommerce.php';
+	require_once 'inc/woocommerce/woocommerce.php';
 }
 
