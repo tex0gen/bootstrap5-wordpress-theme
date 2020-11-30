13b2ec3d05563b5ca34b09d7b2d0897a0108245d
Steve <tex0gen@gmail.com>
2020-11-30 02:08:39 +0000

    Added autoprefixer and css minifier.

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,43 @@
-const path = require("path");
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
-const StyleLintPlugin = require("stylelint-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   context: __dirname,
   entry: {
-    frontend: ["./assets/js/main.js", "./assets/sass/main.scss"],
+    frontend: ['./assets/js/main.js', './assets/sass/main.scss'],
   },
   output: {
-    path: path.resolve(__dirname, "assets/build"),
-    filename: "js/scripts.js",
+    path: path.resolve(__dirname, 'assets/build'),
+    filename: 'js/scripts.js',
     publicPath: './',
   },
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
       {
         test: /\.s[ca]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'resolve-url-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
@@ -62,14 +69,14 @@ module.exports = {
     new ESLintPlugin(),
     new StyleLintPlugin(),
     new BrowserSyncPlugin({
-      files: "**/*.php",
+      files: ['**/*.php'],
     }),
     new MiniCssExtractPlugin({
-      filename: "css/main.min.css",
+      filename: 'css/main.min.css',
     }),
   ],
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
 };
